d47b2429af317cc2330f008e1ee966807465da4a
mgaber <mgaber.alnagar@gmail.com>
2024-02-06 10:54:43 +0200

    main page

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -2,7 +2,7 @@ const main = (req, res) => {
   res.setHeader("Content-Type", "text/html"); // Set content type to HTML
   res.write("<h1>Welcome To Mohamed Gaber Al-Nagar Node API</h1>\n");
   res.write(
-    `<div style="color: red; font-size: 24px;">Postman Collection: <a href='https://documenter.getpostman.com/view/32077555/2s9YywffND'>Postman Collection</a> </div>`
+    `<div style="color: red; font-size: 24px;">Postman Collection: <a href='https://documenter.getpostman.com/view/32077555/2s9YywffND'>Link</a> </div>`
   );
   res.end();
 };
